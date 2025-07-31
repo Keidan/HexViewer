417e5073d592544282f5ecd7efa0980e5a57a120
Keidan (K. Billonneau) <3134348+Keidan@users.noreply.github.com>
2025-07-31 22:23:38 +0200

    Fix import

--- app/src/main/java/fr/ralala/hexviewer/ui/multichoice/GenericMultiChoiceCallback.java
+++ app/src/main/java/fr/ralala/hexviewer/ui/multichoice/GenericMultiChoiceCallback.java
@@ -10,7 +10,6 @@ import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.ListView;
 
