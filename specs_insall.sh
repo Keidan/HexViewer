e87a935bbf461531ce4d6b7df66935f5adffbbc8
Keidan (K. Billonneau) <3134348+Keidan@users.noreply.github.com>
2025-08-04 09:47:43 +0200

    Remaining files that are no longer needed with the previous commit

--- specs_insall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-currentDir=$(pwd)
-if [[ "${currentDir}" != */specs ]]; then
-  cd specs
-fi
-bundle install
-bundle add webrick
--- specs_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-currentDir=$(pwd)
-if [[ "${currentDir}" != */specs ]]; then
-  cd specs
-fi
-bundle exec jekyll serve ${*}
-
-# echo "Goto: http://127.0.0.1:4000/"
